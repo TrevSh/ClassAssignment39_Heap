d8ee0d0528557f4fb10b897daef616d91da2cb33
TrevSh <trevonshelby@outlook.com>
2023-07-29 22:52:05 -0500

    Added comments

--- ClassAssignment39_Heap/ClassAssignment39_Heap.cpp
+++ ClassAssignment39_Heap/ClassAssignment39_Heap.cpp
@@ -16,12 +16,12 @@ int main()
     std::cout << "*pHeap2: "<<*pHeap2<<"\n\n";
 
     std::cout << "Freeing memory pointed to by the pHeap. \n\n";
-    delete pHeap;
+    delete pHeap;               //Everytime you use new, you should also use delete after to free the memory you've used. Otherwise you get memory leaks.
     
     std::cout << "Freeing memory pointed to by the pHeap2. \n\n";
     delete pHeap2;
 
-    //get rid of dangling pointers
+    //get rid of dangling pointers. Dangling pointers point to unused memory, taking up space. The following frees the space by pointing them towards null.
     pHeap = 0;
     pHeap2 = 0;
 
@@ -33,13 +33,13 @@ int* intOnHeap() {
     return pTemp;
 }
 
-//Dont call
+//Dont call. This memory would be lost during runtime. Memory leaks can lead to program crashes.
 void leak1() {
     int* drip1 = new int(30);
 }
 //Dont call
 void leak2() {
     int* drip2 = new int(50);
-    drip2 = new int(100);
+    drip2 = new int(100);       //Moving the pointer from 50 to 100 creates a memory leak, as 50 is not unaccounted for. Notice there are two 'new's and only one delete.
     delete drip2;
 }
